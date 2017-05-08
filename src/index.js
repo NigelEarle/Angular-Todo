fbfd653e53e6702096506c59d95d7ccb93ae581d
nwre87 <nwre87@gmail.com>
2017-05-08 13:25:18 -1000

    add reset/base styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './config';
 import home from './modules/home';
+import './index.scss';
 
 /* @ngInject */
 angular.module('app', [uirouter, home])
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -4,12 +4,10 @@ import uirouter from 'angular-ui-router';
 import routes from './routes';
 import HomeController from './HomeController';
 import HomeService from './HomeService';
-import Tasks from './TasksDirective';
 
 /* @ngInject */
 export default angular.module('home', [uirouter])
   .config(routes)
   .service('HomeService', HomeService)
   .controller('HomeController', HomeController)
-  .directive('tasks', () => new Tasks())
   .name;
